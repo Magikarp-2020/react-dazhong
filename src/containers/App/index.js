c200d0fb5e44cdde2d2d5601fac4d0087f914332
lichao <294645832@qq.com>
2017-10-21 16:54:19 +0800

    目录划分

---
+++ src/containers/App/index.js
@@ -0,0 +1,12 @@
+import {Component} from 'react';
+import Hello from '_containers/Hello/index';
+
+class App extends Component {
+    render() {
+        return (
+            Hello
+        );
+    }
+}
+
+export default App;
