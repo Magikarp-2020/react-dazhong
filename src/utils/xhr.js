41835e6f44c244346df4a1bc69e50c2336e5794a
chao7.li <chao7.li@56qq.com>
2017-11-13 16:13:54 +0800

    xhr

--- src/utils/xhr.js
+++ src/utils/xhr.js
@@ -6,6 +6,16 @@ export default ({url = '', method = 'get', params = {}, data = {}}) => {
         method,
         baseURL: 'http://localhost:8080/',
         params,
-        data
+        data,
+        header: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+        },
+        transformRequest: [function (data) {
+            let ret = '';
+            for (let it in data) {
+                ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            return ret
+        }],
     });
 }
