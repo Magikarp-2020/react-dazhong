61bd51d42c3127be0b6a776d4fd003650747fdf3
chao7.li <chao7.li@56qq.com>
2017-11-16 16:20:45 +0800

    加载更多

--- src/components/LoadMore/index.js
+++ src/components/LoadMore/index.js
@@ -10,7 +10,7 @@ export default class LoadMore extends Component {
 
     render() {
         return (
-            <div className="load-more">
+            <div className="load-more" ref="wrapper">
                 {this.props.isLoadingMore ? <span>加载中...</span> :
                     <span onClick={this.loadMoreHandle.bind(this)}>加载更多</span>}
             </div>
@@ -19,9 +19,33 @@ export default class LoadMore extends Component {
 
     loadMoreHandle() {
         // 执行传递过来的 loadMoreData函数
-        console.log('111');
         if (this.props.loadMoreFn) {
             this.props.loadMoreFn();
         }
+    }
+
+    componentDidMount() {
+        const loadMoreFn = this.props.loadMoreFn;
+        const wrapper = this.refs['wrapper'];
+        let timeoutId = null;
+
+        const callback = () => {
+            const top = wrapper.getBoundingClientRect().top;
+            const windowHeight = window.screen.height;
+            if (top && top < windowHeight) {
+                // 证明 wrapper 已经被滚动到暴露在页面可视范围之内了
+                loadMoreFn()
+            }
+        };
+
+        window.addEventListener('scroll', () => {
+            if (this.props.isLoadingMore) {
+                return;
+            }
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+            timeoutId = setTimeout(callback, 50)
+        }, false);
     }
 }
